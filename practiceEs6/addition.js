a7dc608dccd679600f691d8906bc8d6005d66882
khajaumar <khajau460@gmail.com>
2021-08-21 03:10:12 -0700

    prac

---
+++ practiceEs6/addition.js
@@ -0,0 +1,8 @@
+// const addition  = (x,y) => {
+//     return x+y;
+// }
+// console.log(addition(5,10));
+const bigNum=(num1,num2)=>num1>num2 ? num1 : num2
+
+console.log("Bigger number is",bigNum(78,89));
+ 
