14a4c1fdcdbb63e67ca8c358caf4f52bcfad8d7a
khajaumar <khajau460@gmail.com>
2021-08-21 03:09:55 -0700

    check

---
+++ practiceEs6/DataCheck.js
@@ -0,0 +1,5 @@
+const getName= (fname, lname)=>{
+    return fname + " " + lname ;
+};
+
+console.log (getName("Umar", "Khaja"));
---
+++ practiceEs6/DataFilter.js
@@ -0,0 +1,12 @@
+const arr1 = [1, undefined, "ather", 3, 5, {}, "cat", 5487, 8954, "mat"];
+
+const arr2= [];
+
+arr1.forEach((element) => {
+    if(typeof   element === "string") {
+        arr2.push(element.toUpperCase());
+    }
+    
+});
+
+console.log(arr2);
