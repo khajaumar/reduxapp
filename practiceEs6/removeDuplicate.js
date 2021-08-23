88513d56678e3e752b332a4d781ca6ddfcef00fc
khajaumar <khajau460@gmail.com>
2021-08-23 00:34:32 -0700

    sortDuplicate

---
+++ practiceEs6/removeDuplicate.js
@@ -0,0 +1,9 @@
+// const removeDuplicate=(str)=>{
+//     const arrStr =str.split(" ");
+//     const uniqArr= [...new set(arrStr)];
+//     return uniqArr.join(" ");
+
+// };
+// console.log(removeDuplicate("I am Duplicate. I am good. I want to go"))
+
+console.log("khaja umar is genius")
